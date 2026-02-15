c5767604ba2fe747718d8bb5a1fedd9c880c39fc
Mohit <msms789549@gmail.com>
2026-02-15 12:11:24 +0530

    logo ui

--- app/s/[code]/page.tsx
+++ app/s/[code]/page.tsx
@@ -13,7 +13,7 @@ export default async function Page({
     where: { code },
   });
   if (!url) {
-    return <p>Short URL not found</p>;
+    return <p>Short URL not found...</p>;
   }
   if (url.expiresAt && url.expiresAt < new Date()) {
     return <ExpiredUrlPage originalUrl={url.longUrl} />;
